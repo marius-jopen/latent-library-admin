ae6f97a831df3ff3963aef1694af52d76f08657a
Marius Jopen <mail@mariusjopen.com>
2025-09-02 11:13:43 +0200

    d

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
-export default function Home() {
-  const isAdmin = cookies().get('admin')?.value === '1';
+export default async function Home() {
+  const cookieStore = await cookies();
+  const isAdmin = cookieStore.get('admin')?.value === '1';
   redirect(isAdmin ? '/admin' : '/login');
 }
